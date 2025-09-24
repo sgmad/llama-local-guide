547bfd3bd8366e826f57e61fad0ccd9894c0514f
sgmad <155355629+sgmad@users.noreply.github.com>
2025-09-24 22:45:36 +0800

    Update run_llama_8b.sh

--- scripts/run_llama_8b.sh
+++ scripts/run_llama_8b.sh
@@ -5,7 +5,7 @@ set -e
 
 ./llama.cpp/main \
   -m models/llama-3.1-8b-q4.gguf \
-  -p "Summarize the state of local LLMs in one paragraph." \
+  -p "This is a prompt. Summarize the story of the Lord of the Rings in one paragraph." \
   -n 128 \
   --threads 8 \
   --ctx_size 2048
